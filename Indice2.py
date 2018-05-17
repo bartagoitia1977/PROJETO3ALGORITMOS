8b6b59efb0729d902161be7321ff095dbddcc02c
bartagoitia1977 <33225450+bartagoitia1977@users.noreply.github.com>
2018-05-16 21:42:12 -0300

    Update Indice2.py

--- Indice2.py
+++ Indice2.py
@@ -1,27 +1,39 @@
-class Trie_Knot:
+class Trie_Knot():
 	def __init__(self,item = None,chave = None):
-		self.__item = item
-		self.__chave = chave
-		self.__pointer = [None] * 10
-		self.__mark = False
+		self.item = item
+		self.chave = chave
+		self.pointer = [None] * 10
+		self.mark = False
 		
-class Indice:
+class Indice():
 	def __init__(self):
-		self.__root = Trie_Knot()		
-		
+		self.root = Trie_Knot()
+
 	def inserirItem(self,item,chave):
-		self.__chave = chave
-		self.__item = item
-		for digit in self.__chave:
-			self.__root = Trie_Knot()						
-			
-	def pesquisarItem(self,chave):
-		self.__chave = chave
-		self.__chave = None + self.__chave
-		for digit in self.__chave:
-			
-			
-			
-		
-			
-			
+		knot = self.root
+		for digit in chave:
+			if (knot.pointer[int(digit)] is None):
+				knot.pointer[int(digit)] = Trie_Knot(None, digit)
+				knot = knot.pointer[int(digit)]
+			else:
+				knot = knot.pointer[int(digit)]
+		knot.item = item
+		knot.mark = True
+	
+	def pesquisar(self,chave):
+		knot_pesquisa = self.root
+		for digit in chave:
+			if (knot_pesquisa.pointer[int(digit)] is None):
+				raise KeyError
+			else:
+				knot_pesquisa = knot_pesquisa.pointer[int(None,digit)]
+		if (knot_pesquisa.mark == True):
+			return knot_pesquisa.item
+		else:
+			raise KeyError
+	
+	def __getitem__(self,chave):
+		return self.pesquisar(chave)
+    
+    def __setitem__(self,item,chave):
+        inserirItem(item,chave)
